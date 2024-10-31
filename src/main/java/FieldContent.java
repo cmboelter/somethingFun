f0c94a90798b464973b4422cb4320f9ef8fa24b6
cmboelter <christinambjordan@gmail.com>
2024-10-31 16:19:57 -0400

    getLocation, updateStuffOnBoard, readCommandFromUser, and other random stuff

---
+++ src/main/java/FieldContent.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class FieldContent {
+
+    private final String thing;
+
+    FieldContent(String whatIsIt) {
+        this.thing = whatIsIt;
+    }
+
+    // this is what the field will look like on the board
+    public String getVisualRepresentation() {
+        return thing;
+    }
+
+
+    private static String readCommandFromTheUser(String input) {
+        // do the stuff with the scanner here and check if the character is a valid command (like L for left or whatever you wish)
+        Scanner scanner = new Scanner(System.in);
+        String entry = scanner.nextLine().toLowerCase();
+        // then return the char so that it can be used by the rest of the program
+        return entry;
+
+    }
+    // this is used to update what the board looks like when the command is executed
+    private static void updateStuffOnTheBoard(String command) {
+        // find where the player is on the board; what array location
+        getLocation();
+        // put the player into another field
+        if (.equals("y")) {
+
+
+
+            // replace the old place with e.g. empty floor
+        }
+    }
+    // we run through the matrix and produce one character for every field content
+    private static void printout(FieldContent[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            // the first index in the matrix indicates the row
+            FieldContent[] row = board[i];
+            for (int j = 0; j < row.length; j++) {
+                // for every 'thing' in the row we create an output
+                FieldContent field = row[i];
+                System.out.print(field.getVisualRepresentation());
+            }
+            // after each row we want a new line
+            System.out.println();
+        }
+    }
+
+    private static int[] getLocation(FieldContent[][] board, FieldContent character){
+        int result[] = {-1, -1};
+        for(int i =0; i<board.length; i++){
+            for(int j=0; j <board.length; j++){
+                if(board[i][j] == character){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }return result;
+    }
+
+}
--- src/main/java/MainGame.java
+++ src/main/java/MainGame.java
@@ -1,33 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MainGame {
-    public static void main(String []args)
-    {
-        System.out.println("       ┏━━━━━━━━━━━━━━━━━━━┓");
-        System.out.println("       ┃───────────────────┃");
-        System.out.println("━━━━━━━┛ ║─────────────────┃");
-        System.out.println("         ║─────────────────┃");
-        System.out.println("━━━━━━━┓ ║─────────────────┃");
-        System.out.println("       ┃───────────────────┃");
-        System.out.println("       ┃───────────────────┃");
-        System.out.println("       ┗━━━━━━━━━━━━━━━━━━━┛");
+    public static void main(String[] args) {
+
+        FieldContent[][] board = new FieldContent[8][29];
+        FieldContent blompy1 = new FieldContent("B");
+        FieldContent topLeftC = new FieldContent("┏");
+        FieldContent topRightC = new FieldContent("┓");
+        FieldContent botLeftC = new FieldContent("┗");
+        FieldContent botRightC = new FieldContent("┛");
+        FieldContent borderWall = new FieldContent("━");
+        FieldContent wall = new FieldContent("-");
+        FieldContent door = new FieldContent("║");
+        board[0][0] = topLeftC;
+        board[0][1] = borderWall;
+        board[0][2] = borderWall;
+        board[0][3] = borderWall;
+        board[0][4] = borderWall;
+        System.out.println(board);
+        System.out.println(blompy1);
+
+//        System.out.println("       ┏━━━━━━━━━━━━━━━━━━━┓");
+//        System.out.println("       ┃───────────────────┃");
+//        System.out.println("━━━━━━━┛ ║─────────────────┃");
+//        System.out.println("         ║─────────────────┃");
+//        System.out.println("━━━━━━━┓ ║─────────────────┃");
+//        System.out.println("       ┃───────────────────┃");
+//        System.out.println("       ┃───────────────────┃");
+//        System.out.println("       ┗━━━━━━━━━━━━━━━━━━━┛");
 
 
 
-        Characters blompy1 = new Characters("Blompy", "B");
-        blompy1.setLocation(0, 0, blompy1.getSymbol());
 
 
-        System.out.println(blompy1.getSymbol());
-        System.out.println(Arrays.deepToString(blompy1.getLocation()));
 
 
+
+
+
+        }
 
 
     }
 
 
+//        System.out.println(blompy1.getSymbol());
+//        System.out.println(Arrays.deepToString(blompy1.getLocation()));
 
 
-};
+//    };
+
+
+
+
 
