1440356253cc6ca72e6af91fd2b5c0d03abcca1f
cmboelter <christinambjordan@gmail.com>
2024-11-10 13:35:22 -0500

    updates w/Tom2

--- src/main/java/MainGame.java
+++ src/main/java/MainGame.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 public class MainGame {
     public static void main(String[] args) {
 
-        FieldContent[][] board = new FieldContent[8][29];
+        FieldContent[][] board = new FieldContent[2][2];
         FieldContent blompy1 = new FieldContent("B");
-        FieldContent topLeftC = new FieldContent("┏");
-        FieldContent topRightC = new FieldContent("┓");
-        FieldContent botLeftC = new FieldContent("┗");
-        FieldContent botRightC = new FieldContent("┛");
+        FieldContent topLeftCorner = new FieldContent("┏");
+        FieldContent topRightCorner = new FieldContent("┓");
+        FieldContent botLeftCorner = new FieldContent("┗");
+        FieldContent botRightCorner = new FieldContent("┛");
         FieldContent borderWall = new FieldContent("━");
         FieldContent wall = new FieldContent("-");
         FieldContent door = new FieldContent("║");
 
-        board[0][0] = topLeftC;
+        board[0][0] = topLeftCorner;
         board[0][1] = borderWall;
-        board[0][2] = borderWall;
-        board[0][3] = borderWall;
-        board[0][4] = borderWall;
+        board[1][0] = borderWall;
+        board[1][1] = borderWall;
+//        board[0][4] = borderWall;
 
         printout(board);
 
@@ -50,12 +50,12 @@ public class MainGame {
 
         }
     private static void printout(FieldContent[][] board) {
-        for (int i = 0; i < board.length; i++) {
+        for (int rowNumber = 0; rowNumber < board.length; rowNumber++) {
             // the first index in the matrix indicates the row
-            FieldContent[] row = board[i];
-            for (int j = 0; j < row.length; j++) {
+            FieldContent[] row = board[rowNumber];
+            for (int columnNumber = 0; columnNumber < row.length; columnNumber++) {
                 // for every 'thing' in the row we create an output
-                FieldContent field = row[i];
+                FieldContent field = row[columnNumber];
                 if(field == null){
                     System.out.print(".");
                 } else {
