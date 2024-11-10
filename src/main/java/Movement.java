296ef8bb32f263c40c0d8dc47e0826cb3d38d419
cmboelter <christinambjordan@gmail.com>
2024-11-10 13:35:48 -0500

    fixed printout

---
+++ src/main/java/Movement.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Movement {
+
+
+
+
+
+//get current location of character
+//    int currentLocation = movementArr.get(i).get(j);
+//
+
+//
+//        while(){
+//        case "y":
+//
+//        };
+//        reader.close();
+//
+//
+//    public Movement() throws IOException {
+//    }
+//
+//    public void getCharacterLocation(){
+//
+//        }
+    }
+
+
+
+
+
+
+
+
+
