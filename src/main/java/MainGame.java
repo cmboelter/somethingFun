ababa83667894e55b30bc6cb606651a70651ea97
cmboelter <christinambjordan@gmail.com>
2024-12-30 14:27:26 -0500

    refactor work with events for wisdom/courage

--- src/main/java/MainGame.java
+++ src/main/java/MainGame.java
@@ -278,6 +278,21 @@ public class MainGame {
     }
 
 
+    }
+    public void wisdomAndCourage(int blompyRow, int blompyColumn, int wisdom, Map<String,Integer> blompyCharacterTraits, int randNumColumn, ){
+        if(blompyRow == 9 && blompyColumn == 14){
+            System.out.println("You've eaten an enlightened jelly fruit and now you're wiser.");
+            blompyCharacterTraits.put("Wisdom", wisdom +1);
+        } else if(blompyRow == 8 && blompyColumn == randNumColumn){
+            System.out.println("An old wise owl passed along some wisdom. +1");
+            blompyCharacterTraits.put("Wisdom", wisdom +1);
+        } else if(blompyRow == 8 && blompyColumn == randNumColumn){
+            System.out.println("An old wise owl passed along some wisdom. +1");
+            blompyCharacterTraits.put("Wisdom", wisdom +1);
+    }
+
+
+
 
 
 
@@ -499,6 +514,9 @@ public int[] whereDoesBlompyWannaGo(FieldContent[][] board, FieldContent blompy1
             board[blompyRow][blompyColumn - 1] = null;
         }
     }
+
+
+
 }
 
 
